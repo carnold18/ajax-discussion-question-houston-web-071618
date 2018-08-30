21e993e3732acb22a66bfe059b3884c900a7a02b
Christopher Arnold <chrisarnold2010@gmail.com>
2018-08-30 10:32:27 -0500

    group discussion complete

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ const fullname = document.getElementById("fullname");
 console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
 
 document.addEventListener("DOMContentLoaded", () => {
+  fetch('https://randomuser.me/api/')
+  .then( res => res.json() )
+  .then( data => {
+  	debugger
+    console.log(data) 
+  })
   console.log("CONTENT LOADED!");
 });
+
+
